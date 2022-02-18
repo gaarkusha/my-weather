ef2d2aec3bd71d0191f17abab6754e586bf29e28
gaarkusha <svladgarkushaal@mail.ru>
2022-02-18 15:53:19 +0300

    Initial commit: version 1.0

--- src/index.tsx
+++ src/index.tsx
@@ -5,14 +5,18 @@ import reportWebVitals from './reportWebVitals';
 import './Styles/index.scss'
 import {BrowserRouter as Router} from "react-router-dom";
 import {ThemeProvider} from "./Provider/ThemeProvider";
+import {Provider} from "react-redux";
+import {store} from "./store/store";
 
 ReactDOM.render(
     <React.StrictMode>
-        <Router>
-            <ThemeProvider>
-                <App/>
-            </ThemeProvider>
-        </Router>
+        <Provider store={store}>
+            <Router>
+                <ThemeProvider>
+                    <App/>
+                </ThemeProvider>
+            </Router>
+        </Provider>
     </React.StrictMode>,
     document.getElementById('root')
 );
